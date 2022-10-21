1ef75565bf1e546a698d760f418f58894c17957a
nxtera <64105557+nxtera@users.noreply.github.com>
2022-10-22 00:45:07 +0100

    Added view by budget and view by department functions.
    On branch main
    Changes to be committed:
    	modified:   index.js
    	modified:   utils/func.js

--- index.js
+++ index.js
@@ -1,82 +1,99 @@
-const inquirer = require('inquirer');
-const db = require('./db/connection')
-const logo = require('asciiart-logo');
-const config = require('./package.json');
-var colors = require('colors');
+const inquirer = require("inquirer");
+const db = require("./db/connection");
+const logo = require("asciiart-logo");
+const config = require("./package.json");
+var colors = require("colors");
 
 // Import functions from the func file.
 const {
-    viewEmployees,
-    viewRoles,
-    addEmployee,
-    addRole,
-    addDepartment,
-    updateEmployeeRole,
-    viewDepartments
-} = require('./utils/func')
+  viewEmployees,
+  viewRoles,
+  addEmployee,
+  addRole,
+  addDepartment,
+  updateEmployeeRole,
+  viewDepartments,
+  viewByManager,
+  viewByDepartment,
+  viewByBudget,
+} = require("./utils/func");
 
 // Main menu prompt that displays all functions to the user
 const mainmenu = () => {
-    console.log('i like cake and pies'.underline.red); // outputs red underlined text
-    inquirer.prompt({
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'menu',
-        choices: [
-            'View All Employees',
-            'Add Employee',
-            'Update Employee Role',
-            'View All Roles',
-            'Add Role',
-            'View All Departments',
-            'Add Department',
-            'Exit'
-        ]
+  console.log("i like cake and pies".underline.red); // outputs red underlined text
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      name: "menu",
+      choices: [
+        "View All Employee\'s",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "View Employee by Manager",
+        "View Employees by Department",
+        "View Budget Total for Each Department",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      // Switch statement to start the function the user has selected
+      switch (answer.menu) {
+        case "View All Employee\'s":
+          viewEmployees();
+          break;
 
-    }).then((answer) => {
+        case "Add Employee":
+          addEmployee();
+          break;
 
-// Switch statement to start the function the user has selected
-        switch (answer.menu) {
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
 
-            case 'View All Employees':
-                viewEmployees()
-                break;
+        case "View All Roles":
+          console.log("...\n");
+          viewRoles();
+          break;
 
-            case 'Add Employee':
-                addEmployee();
-                break;
+        case "Add Role":
+          addRole();
+          break;
 
-            case 'Update Employee Role':
-                updateEmployeeRole();
-                break;
+        case "View All Departments":
+          console.log("...\n");
+          viewDepartments();
+          break;
 
-            case 'View All Roles':
-                console.log('...\n');
-                viewRoles();
-                break;
+        case "Add Department":
+          addDepartment();
+          break;
 
-            case 'Add Role':
-                addRole();
-                break;
+        case "View Employee by Manager":
+          viewByManager();
+          break;
 
-            case 'View All Departments':
-                console.log('...\n');
-                viewDepartments();
-                break;
+        case "View Employees by Department":
+          viewByDepartment();
+          break;
 
-            case 'Add Department':
-                addDepartment();
-                break;
+        case "View Budget Total for Each Department":
+            viewByBudget();
+          break;
 
-            case 'Exit':
-                db.end();
-                break;
-        }
+        case "Exit":
+          db.end();
+          break;
+      }
     });
-}
+};
 
 // Call to initialize app
 console.log(logo(config).render().brightMagenta);
 mainmenu();
 
-exports.mainmenu = mainmenu
+exports.mainmenu = mainmenu;
